06bb2b79f55d5a0b14edabb3b6c3c649c1a8d45e
Randy Layne <randy.layne@teamtreehouse.com>
2021-07-01 22:35:55 -0400

    Initial commit

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req, res) => {
+  res.send("Well hello there.");
+});
+
+export { router as currentUserRouter };
